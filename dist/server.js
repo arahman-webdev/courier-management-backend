8c9fcfff022d16efbfb25789bdbd5876a4b9a82e
arahman225 <upworkabdurrahman@gmail.com>
2025-07-29 08:49:17 +0600

    set up MongoDB connection and environment config

---
+++ dist/server.js
@@ -0,0 +1,7 @@
+"use strict";
+const x = 10;
+console.log("sdlg");
+/**
+ * userName: courier_management
+ * pass: L8wqDHaqdWCzKOsx
+ */ 
---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express"
+import cors from "cors"
+import cookieParser from 'cookie-parser'
+
+
+export const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+
+app.get('/', (req:Request, res:Response)=>{
+
+    res.send("Hi! world!")
+})
+
+
